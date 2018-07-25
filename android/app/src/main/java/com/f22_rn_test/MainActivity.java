978204e266f66d70bd4744e03eb92a0db1d0bb9f
Siddharth Srinivasan <siddharth@f22labs.com>
2018-07-25 16:22:54 +0530

    first commit

---
+++ android/app/src/main/java/com/f22_rn_test/MainActivity.java
@@ -0,0 +1,15 @@
+package com.f22_rn_test;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "F22_RN_Test";
+    }
+}
